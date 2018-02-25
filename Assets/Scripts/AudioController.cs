04693abd550e82151401a1af1900852bd3e4bb1b
Saman <arifs@uwindsor.ca>
2018-02-25 02:55:03 -0500

    First Commit

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public const string audioName = "mario.wav";
+
+    [Header("Audio Stuff")]
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+    public string soundPath;
+
+    private void Awake()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        soundPath = "file://" + Application.streamingAssetsPath + "/Sound/";
+        StartCoroutine(LoadAudio());
+    }
+
+    private IEnumerator LoadAudio()
+    {
+        WWW request = GetAudioFromFile(soundPath, audioName);
+        yield return request;
+
+        audioClip = request.GetAudioClip();
+        audioClip.name = audioName;
+
+        PlayAudioFile();
+    }
+
+    private void PlayAudioFile()
+    {
+        audioSource.clip = audioClip;
+        audioSource.Play();
+        audioSource.loop = true;
+    }
+
+    private WWW GetAudioFromFile(string path, string filename)
+    {
+        string audioToLoad = string.Format(path + "{0}", filename);
+        WWW request = new WWW(audioToLoad);
+        return request;
+    }
+}
